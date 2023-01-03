9a1318124b60a94a501742e661f118e921e2070b
unknown <artemlitvinov81@gmail.com>
2023-01-04 00:36:22 +0300

    header - drop-menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
         'base-black': '#171717',
         'base-white': '#F8F8F8',
         'base-red': '#FF304D',
+        'base-gray': '#919191',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
       },
     },
   },
